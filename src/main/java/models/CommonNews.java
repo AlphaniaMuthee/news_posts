652c6acbec9af1afa6192229180d1432c6172fa1
AlphaniaMuthee <walphania2@gmail.com>
2020-05-10 15:42:08 +0300

    add common news class for non-departmental specific news

---
+++ src/main/java/models/CommonNews.java
@@ -0,0 +1,12 @@
+package models;
+
+public class CommonNews {
+    private String news;
+    private String writer;
+    private int id;
+
+    public CommonNews(String news, String writer) {
+        this.news = news;
+        this.writer = writer;
+    }
+}
