45e9df358266be7e9a919a5a4423bde7b9b9d546
AlphaniaMuthee <walphania2@gmail.com>
2020-05-10 17:07:39 +0300

    add tests for common news

--- src/main/java/models/CommonNews.java
+++ src/main/java/models/CommonNews.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class CommonNews {
     private String news;
     private String writer;
@@ -8,5 +10,44 @@ public class CommonNews {
     public CommonNews(String news, String writer) {
         this.news = news;
         this.writer = writer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CommonNews)) return false;
+        CommonNews that = (CommonNews) o;
+        return id == that.id &&
+                news.equals(that.news) &&
+                writer.equals(that.writer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(news, writer, id);
+    }
+
+    public String getNews() {
+        return news;
+    }
+
+    public void setNews(String news) {
+        this.news = news;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
