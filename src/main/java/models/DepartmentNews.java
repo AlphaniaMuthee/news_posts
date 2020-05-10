4977d8dc3765792209a0dbb945dcee73f78cb8a3
AlphaniaMuthee <walphania2@gmail.com>
2020-05-10 17:13:31 +0300

    add department news class for department specific info

---
+++ src/main/java/models/DepartmentNews.java
@@ -0,0 +1,18 @@
+package models;
+
+public class DepartmentNews {
+    private int sectionId;
+
+    public DepartmentNews(int sectionId) {
+        super(news, author);
+        this.sectionId = sectionId;
+    }
+
+    public int getSectionId() {
+        return sectionId;
+    }
+
+    public void setSectionId(int sectionId) {
+        this.sectionId = sectionId;
+    }
+}
