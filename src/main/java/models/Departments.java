e66f2e13ac1a0972fe035c0f90eb4af59ddf375f
AlphaniaMuthee <walphania2@gmail.com>
2020-05-10 14:34:04 +0300

    add the tests file for departments

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,4 +1,14 @@
 package models;
 
 public class Departments {
+    private String name;
+    private String details;
+    private int totalEmployees;
+    private int id;
+
+    public Departments(String name, String details, int totalEmployees) {
+        this.name = name;
+        this.details = details;
+        this.totalEmployees = totalEmployees;
+    }
 }
